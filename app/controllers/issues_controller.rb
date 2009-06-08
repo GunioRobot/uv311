10542ed396da5424c3dd87987f2bc35b76cf9761
ksteigerwald <kris@forwardfootmedia.com>
2009-06-07 23:40:23 -0400

    count

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,7 +1,7 @@
 class IssuesController < ApplicationController
   
   def index
-    @issues=Issue.find(:all, :limit => 5)
+    @issues=Issue.find(:all, :limit => 15)
   end 
   
   def update
@@ -10,21 +10,26 @@ class IssuesController < ApplicationController
 
   def vote
      @issue=Issue.find_by_id(params[:id])
+     firstcount= @issue.votes.count
      if !@issue.nil?
        Vote.find_or_create_by_issue_id_and_user_id(params[:id], current_user.id)
-       raise @issue.votes.count.to_yaml
-       return @issue.votes.count
+       # raise @issue.votes.count.to_yaml
+       count= @issue.votes.count
+       status=count>firstcount ? 202 :200        
      else
        #No issue found
        return false
      end
-   
+  
 
-    respond_to do |format|
-        format.js { head :ok}
-        format.xml  { head :ok }
-    end
-    
+     respond_to do |format|
+       if count
+         flash[:notice] = 'count was successfully updated.'
+         format.js { head status}
+         format.xml  { head :ok }
+       end
+     end
+     
     # format.json  { render :json => @asset.to_json(:include => :subject, :methods => :icon_for), :status => status }
   end
   
