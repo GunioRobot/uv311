a61ec517f047dd2a9176d36395e3f95daaaf3882
ksteigerwald <kris@forwardfootmedia.com>
2009-06-08 00:31:45 -0400

    create issue link

--- app/javascripts/dc311.js
+++ app/javascripts/dc311.js
@@ -1,9 +1,5 @@
 //add your js code here
 
-$("p").live("click", function(){
-  $(this).after("<p>Another paragraph!</p>");
-});
-
 jQuery(document).ready(function() {
   jQuery("#issue").autocomplete(autocomplete);
   jQuery('a[rel*=facebox]').facebox();
