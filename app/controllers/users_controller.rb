a7e90a2e1b07e64b170204174ee19dcac63e9ba1
visualcv_christophe <christophe.lucas@visualcv.com>
2009-06-08 00:45:48 -0400

    fix users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
    
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+
   def show
     @user = User.find(params[:id])
 
