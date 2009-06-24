7babe79888bc76fca9cd6784792ea524e0d4a53d
Guille Carlos <g@guillecarlos.com>
2009-06-23 21:38:12 -0400

    trying to fix heroku bug

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -115,6 +115,6 @@ class Issue < ActiveRecord::Base
   end
   
   def issues_with_address(address)
-    @issues = Issue.find(:all, :conditions => ['address LIKE LOWER(?) OR title LIKE LOWER(?)', "%#{address}%", "%#{address}%"])
+    @issues = Issue.find(:all, :conditions => ['address ILIKE ? OR title ILIKE ?', "%#{address}%", "%#{address}%"])
   end
 end
