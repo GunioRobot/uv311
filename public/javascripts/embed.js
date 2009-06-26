e49f1848d067c14ceb365bdf863291bd3c499bd7
Guille Carlos <g@guillecarlos.com>
2009-06-26 19:00:38 -0400

    Did some work on the embeded js

--- public/javascripts/embed.js
+++ public/javascripts/embed.js
@@ -11,15 +11,25 @@ function uv311_loadStylesheet(url) {
   var css = document.createElement('link');
   css.setAttribute('rel', 'stylesheet');
   css.setAttribute('href', url);
-  document.body.appendChild(css);
+  document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+function facebox_loadStylesheet(url) {
+	var css = document.createElement('link');
+	css.setAttribute('rel', 'stylesheet');
+	css.setAttribute('media', 'screen');
+	css.setAttribute('href', url);
+	css.setAttribute('type', 'text/css');
+	/*document.body.appendChild(css);*/
+	document.getElementsByTagName("head")[0].appendChild(css);
 }
  
 uv311_loadScript("http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js");
 uv311_loadScript("http://www.easy311.org/javascripts/jquery.js");
 uv311_loadScript("http://www.easy311.org/javascripts/facebox.js");
-uv311_loadStylesheet("http://www.easy311.org/stylesheets/facebox.css");
+facebox_loadStylesheet("http://www.easy311.org/stylesheets/facebox.css");
 uv311_loadStylesheet("http://www.easy311.org/stylesheets/embed.css");
-document.getElementById('uv311').innerHTML='<div id="uv311"><a href="easy311.html" rel="facebox">easy311.org</a><div id="uv311prompt" style="display: none"><p>I want to...</p><input type="text" id="uv311searchstring"><input type="button" id="uv311submit" value="search"></div><div id="uv311searchresults"></div></div></div>';
+document.getElementById('uv311').innerHTML='<div style="width: 67px;"><a href="easy311.html" rel="facebox"><div id="uv311"></div></a></div>';
  
 window.onload = function() {
   $("#uv311submit").click(function() {
