5409024b3539efaab91909d1058d76356e3037e0
Guille Carlos <g@guillecarlos.com>
2009-06-25 23:28:18 -0400

    Added facebox for embeded

--- public/javascripts/embed.js
+++ public/javascripts/embed.js
@@ -15,14 +15,25 @@ function uv311_loadStylesheet(url) {
 }
  
 uv311_loadScript("http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js");
+uv311_loadScript("http://www.easy311.org/javascripts/jquery.js");
+uv311_loadScript("http://www.easy311.org/javascripts/facebox.js");
+uv311_loadStylesheet("http://www.easy311.org/stylesheets/facebox.css");
 uv311_loadStylesheet("http://www.easy311.org/stylesheets/embed.css");
-document.getElementById('uv311').innerHTML='<div id="uv311"><a href="#" onclick="$(\'#uv311prompt\').fadeIn(\'fast\');">easy311.org</a><div id="uv311prompt" style="display: none"><p>I want to...</p><input type="text" id="uv311searchstring"><input type="button" id="uv311submit" value="search"></div><div id="uv311searchresults"></div></div></div>';
+document.getElementById('uv311').innerHTML='<div id="uv311"><a href="easy311.html" rel="facebox">easy311.org</a><div id="uv311prompt" style="display: none"><p>I want to...</p><input type="text" id="uv311searchstring"><input type="button" id="uv311submit" value="search"></div><div id="uv311searchresults"></div></div></div>';
  
 window.onload = function() {
   $("#uv311submit").click(function() {
     alert("hi");
     return false;
-  });
-  
+  });  
+};
+
+window.onload = function() {    
+	jQuery(document).ready(function($) {
+      $('a[rel*=facebox]').facebox({
+        loading_image : 'loading.gif',
+        close_image   : 'closelabel.gif'
+      }) 
+    })
 };
 
